0a80604025df3cf8b73ec2b847e5724858b178f6
ariaprstyarsndr <ariaprasetyarisnandar@gmail.com>
2023-12-23 02:11:11 +0700

    test push git

---
+++ variabel/variabel.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName string = "Kenji"
+
+	var lastName string = "Ferdian"
+
+	fmt.Println(firstName, lastName)
+	fmt.Printf("Halo, Kenji Ferdian\n")
+	fmt.Printf("Halo, %s %s \n", firstName, lastName)
+
+	var ()
+}
